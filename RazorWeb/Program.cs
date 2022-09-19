f64b16b3b0bd2ce615c40d1f312c0f59d15344aa
NguyenMinhQuan201 <quannm2812@gmail.com>
2022-09-19 15:59:46 +0700

    success

--- RazorWeb/Program.cs
+++ RazorWeb/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
